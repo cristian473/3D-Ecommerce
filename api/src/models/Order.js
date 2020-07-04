2a7296ca68858dba4b71f326082fbb742d9e526a
aguspagano <aguspagano@gmail.com>
2020-07-04 19:45:19 -0300

    Update Order.js

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -17,17 +17,4 @@ const Order = (sequelize, S) => {
     return O;
 };
 
-  const C = sequelize.define("order", {
-    id: {
-      type: S.INTEGER,
-      allowNull: true,
-      autoIncrement: true,
-      primaryKey: true
-    },
-  });
-
-  return C;
-};
-
-
 module.exports = Order;
