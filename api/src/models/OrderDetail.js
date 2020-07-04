baad1c47b130add469f0000b2c1f12ce6a398b64
aguspagano <aguspagano@gmail.com>
2020-07-04 19:46:38 -0300

    Update OrderDetail.js

--- api/src/models/OrderDetail.js
+++ api/src/models/OrderDetail.js
@@ -2,22 +2,8 @@ var Sequelize = require('sequelize');
 const S = Sequelize;
 const OrderDetail = (sequelize, S) => {
 
-    const OD = sequelize.define("orderDetails", {
-        id: {
-            type: S.INTEGER,
-            allowNull: true,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        qty: { type: S.REAL },
-        price: { type: S.REAL }
-    });
-    return OD;
-};
-module.exports = OrderDetail;
-
-  const OL = sequelize.define("order_details", {
-    id: {
+const OL = sequelize.define("order_details", {
+    orderId: {
       type: S.INTEGER,
       allowNull: true,
       autoIncrement: true,
